22520f541ce3079e198b4685eaff7ed2f4fada70
kyorohiro <kyorohiro@gmail.com>
2018-02-15 03:55:16 +0900

    add clipRect example

---
+++ clipRect/web/main.dart
@@ -0,0 +1,15 @@
+//
+//
+// WebGL entry point
+//
+//
+
+import 'package:umiuni2d_sprite_html5/umiuni2d_sprite.dart';
+import 'package:example/primitive_test.dart';
+
+void main() {
+  GameWidget game = new GameWidget();
+  game.stage.root.addChild(new PrimitiveTest());
+  game.start(useAnimationLoop: true);
+}
+
